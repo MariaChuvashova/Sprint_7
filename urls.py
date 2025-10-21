d7b86b76a8d20669fc480361047591c4d0ef76d2
Maria Chuvashova <chuvashova_maria@mail.ru>
2025-10-21 23:31:45 +0300

    Initial commit: README only

--- urls.py
+++
@@ -1,9 +0,0 @@
-BASE_URL = 'https://qa-scooter.praktikum-services.ru/api/v1'
-
-class URLs:
-    CREATE_COURIER = f'{BASE_URL}/courier'
-    LOGIN_COURIER = f'{BASE_URL}/courier/login'
-    DELETE_COURIER = f'{BASE_URL}/courier/'
-    CREATE_ORDER = f'{BASE_URL}/orders'
-    GET_ORDERS_LIST = f'{BASE_URL}/orders'
-    CANCEL_ORDER = f'{BASE_URL}/orders/cancel'
